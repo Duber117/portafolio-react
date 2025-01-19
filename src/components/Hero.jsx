ca92e1a2af6322a97bc3e2e53c89b42489db0579
Dxp117 <dubervalderrama8@gmail.com>
2025-01-18 19:59:49 -0500

    fix: Corregir la extensión de la imagen del avatar en el componente Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                 <div>
                     <div className="flex items-center gap-3">
                         <figure className="img-box w-9 h-9 rounded-lg">
-                            <img src="/images/avatar-1.jpg" width={40} height={40} alt="Dxp-117" className="img-cover" />
+                            <img src="/images/avatar-1.jpeg" width={40} height={40} alt="Dxp-117" className="img-cover" />
                         </figure>
                         <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
                             <span className="relative w-2 h-2 rounded-full bg-emerald-400">
